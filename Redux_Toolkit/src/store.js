a6885e2d622a9d44dd9a6cecd5710f9823a40bca
jasmine0714 <woooooooow22@gmail.com>
2023-04-23 21:06:40 +0900

    ✨ createAction으로 기존 addToDo/deleteToDo 대체

--- Redux_Toolkit/src/store.js
+++ Redux_Toolkit/src/store.js
@@ -1,22 +1,25 @@
 import { legacy_createStore } from "redux";
 import { createAction } from "@reduxjs/toolkit";
 
-const ADD = "ADD";
-const DELETE = "DELETE";
+const addToDo = createAction("ADD");
+const deleteToDo = createAction("DELETE");
 
-const addToDo = (text) => {
-  return {
-    type: ADD,
-    text,
-  };
-};
+// const ADD = "ADD";
+// const DELETE = "DELETE";
 
-const deleteToDo = (id) => {
-  return {
-    type: DELETE,
-    id: parseInt(id),
-  };
-};
+// const addToDo = (text) => {
+//   return {
+//     type: ADD,
+//     text,
+//   };
+// };
+
+// const deleteToDo = (id) => {
+//   return {
+//     type: DELETE,
+//     id: parseInt(id),
+//   };
+// };
 
 const reducer = (state = [], action) => {
   switch (action.type) {
