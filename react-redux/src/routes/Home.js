9d9b75b2c63a4dd36a223285106e93f6998b1383
jasmine0714 <woooooooow22@gmail.com>
2023-04-23 13:03:39 +0900

    ✨mapStateToProps : connect로 state을 Home에 prop으로 전달

--- react-redux/src/routes/Home.js
+++ react-redux/src/routes/Home.js
@@ -1,1 +1,30 @@
-export default () => "Home";
+import React from "react";
+import { useState } from "react";
+import { connect } from "react-redux";
+
+function Home({ toDos }) {
+  const [text, setText] = useState("");
+  const onChange = (e) => {
+    setText(e.target.value);
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+    setText("");
+    console.log(text);
+  };
+  return (
+    <>
+      <h1>To Do</h1>
+      <form onSubmit={onSubmit}>
+        <input type='text' value={text} onChange={onChange}></input>
+        <button>Add</button>
+      </form>
+      <ul>{JSON.stringify(toDos)}</ul>
+    </>
+  );
+}
+
+function mapStateToProps(state) {
+  return { toDos: state };
+}
+export default connect(mapStateToProps)(Home);
