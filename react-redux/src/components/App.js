402f9d4860cafe302293556b7547e69aec774969
jasmine0714 <woooooooow22@gmail.com>
2023-04-23 13:01:33 +0900

    🚚 Home/Detail 라우팅

--- react-redux/src/components/App.js
+++ react-redux/src/components/App.js
@@ -1,5 +1,17 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Detail from "../routes/Detail";
+import Home from "../routes/Home";
+
 function App() {
-  return <div>hi</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' exact element={<Home />}></Route>
+        <Route path='/:id' element={<Detail />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
